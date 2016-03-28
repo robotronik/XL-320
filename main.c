a48d80666342f1761fda5ad93d2ac51ef54102e4
larchuto <johan.lafon@phelma.grenoble-inp.fr>
2016-03-28 12:48:09 +0200

    remove some useless stuff

--- main.c
+++ main.c
@@ -16,8 +16,5 @@ int main(void)
 			printf("\\x%08x",instr_buff[i]);
 	}
 	printf("\n");
-	printf("0x%08x\n",test.LEN_H);
-	uint8_t data_blk[11]={0xFF,0xFF,0xFD,0x00,0xFE,0x06,0x00,0x03,0x25,0x00,0x03};
-	printf("0x%08x\n",update_crc(0,data_blk,11));
 	return 0;
 }
