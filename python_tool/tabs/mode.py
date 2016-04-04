dc5d982408a5bf6a3ec017d97b8a8bcee0636bdb
larchuto <johan.lafon@phelma.grenoble-inp.fr>
2016-04-04 20:48:31 +0200

    mode ajouté mais NOK ??

--- python_tool/tabs/mode.py
+++ python_tool/tabs/mode.py
@@ -6,12 +6,21 @@ class ModeTab(ttk.Frame):
 		ttk.Frame.__init__(self, parent)
 		self.parent=parent
 		self.initialize()
+		self.mode_values={}
+		self.mode_values['Wheel']=1
+		self.mode_values['Join']=2
 
 	def initialize(self):
 		self.grid()
+
+		self.choice_list=ttk.Combobox(self)
+		self.choice_list.config(values=['Wheel','Join'])
+		self.choice_list.grid(column=0,row=0)
+
 		self.buttonSend=ttk.Button(self,text='Set Mode',command=self.OnButtonClick_Send)
-		self.buttonSend.grid(column=0,row=0)
+		self.buttonSend.grid(column=0,row=1)
 
 	def OnButtonClick_Send(self):
-		# libxl320=cdll.LoadLibrary("./libxl320.so")
+		mode=self.mode_values[self.choice_list.get()]
+		self.parent.lib.set_control_mode_servo(byref(self.parent.servo),c_uint(mode),c_uint(1))
 		pass
