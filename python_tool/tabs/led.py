f8ccf6ac0a20fe091658e7024a6fd76573936cc3
larchuto <johan.lafon@phelma.grenoble-inp.fr>
2016-04-04 19:58:10 +0200

    partie led fonctionnelle

--- python_tool/tabs/led.py
+++ python_tool/tabs/led.py
@@ -7,12 +7,29 @@ class LEDColorTab(ttk.Frame):
 		ttk.Frame.__init__(self, parent)
 		self.parent=parent
 		self.initialize()
+		self.color_values={}
+		self.color_values['off']=0
+		self.color_values['red']=1
+		self.color_values['green']=2
+		self.color_values['yellow']=3
+		self.color_values['blue']=4
+		self.color_values['pink']=5
+		self.color_values['blue green']=6
+		self.color_values['white']=7
+
 
 	def initialize(self):
 		self.grid()
+
+		self.choice_list=ttk.Combobox(self)
+		self.choice_list.config(values=['off','red','green','yellow','blue',
+										'pink','blue green','white'])
+		self.choice_list.grid(column=0,row=0)
+
 		self.buttonSend=ttk.Button(self,text='Set LED Color',command=self.OnButtonClick_Send)
-		self.buttonSend.grid(column=0,row=0)
+		self.buttonSend.grid(column=0,row=1)
 
 	def OnButtonClick_Send(self):
-		self.parent.lib.set_led_color_servo(byref(self.parent.servo),2,1)
+		color=self.color_values[self.choice_list.get()]
+		self.parent.lib.set_led_color_servo(byref(self.parent.servo),c_uint(color),1)
 		pass
