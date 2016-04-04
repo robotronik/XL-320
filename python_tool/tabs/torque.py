53ff06ae79071497d5840fe45635568c7a989f08
larchuto <johan.lafon@phelma.grenoble-inp.fr>
2016-04-04 21:07:55 +0200

    torque ok

--- python_tool/tabs/torque.py
+++ python_tool/tabs/torque.py
@@ -11,11 +11,11 @@ class TorqueTab(ttk.Frame):
 
 	def initialize(self):
 		self.grid()
-		self.IDValueSpinBox=tkinter.Spinbox(self,from_=0,to=1023)
-		self.IDValueSpinBox.grid(column=0,row=0)
+		self.torqueValueSpinBox=tkinter.Spinbox(self,from_=0,to=1023)
+		self.torqueValueSpinBox.grid(column=0,row=0)
 		buttonSend=ttk.Button(self,text='Set Torque Limit',command=self.OnButtonClick_Send)
 		buttonSend.grid(column=0,row=1)
 
 	def OnButtonClick_Send(self):
-		libxl320=cdll.LoadLibrary("./libxl320.so")
-		print(self.IDValueSpinBox.get())
+		torque=int(self.torqueValueSpinBox.get())
+		self.parent.lib.set_torque_servo(byref(self.parent.servo),c_uint(torque),c_uint(1))
